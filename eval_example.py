28c715d8dcd281f92b89f7772512932be5c8e2f0
rafaelsandroni <rafaelsandroni@users.noreply.github.com>
2024-08-27 23:09:04 +0200

    Describe client usage

--- eval_example.py
+++ eval_example.py
@@ -29,12 +29,11 @@ def call_llm(messages):
 
 #     logging.info(f"Added {application.get("policies", [])} policies")
 
-
-
-# fet the current time
+# set the current time
 now = datetime.now()
 time_of_day = now.strftime("%I:%M %p")
 
+# set up the application first
 application_id = "YOUR_APPLICATION_ID"
 system_prompt = f"You're a helfull assistant. Current time is {time_of_day}. You work for ACME company. Act as a customer support agent to help users to create an investment account."
 client.add_application(application_id=application_id, system_prompt=system_prompt)
@@ -47,38 +46,42 @@ policy_1 = dict(
     After 6:00 PM and before 9:00 PM, say "Good evening."
     After 9:00 PM and before 1:00 AM, say "Good night.""",
     examples=[dict(
-        output_example="Saying 'Good morning!' when current time is after 6:00PM",
-        type="FAIL"
-        ),
-        dict(
-            output_example="Hi!",
+            output_example="Saying 'Good morning!' when current time is after 6:00PM", # could add example as an instruction
             type="FAIL"
         ),
         dict(
-            output_example="Hi, good evening!",
+            output_example="Hi!", # or as a message example
+            type="FAIL"
+        ),
+        dict(
+            output_example="Hi, good evening!", # another message example
             type="PASS"
         )   
     ],
-    override_response=None,# could add a default response here
+    override_response=None, # could add a default response here, when it policy fails, this response is returned
 )
 
+# then set up the policy
 for p in [policy_1]:
     client.add_policy(
         **p
     )
 
+# user input
 messages = [
     {"role": "user", "content": "Hi! I would like to create an investment account."},
 ]
 
 logging.info(f"User input: {json.dumps(messages[-1], indent=4)}")
 
+# generate an ai response
 assistant_response = call_llm(messages)
 
 messages.append({"role": "assistant", "content": assistant_response})
 
 logging.info(f"Assistant output: {json.dumps(messages[-1], indent=4)}")
 
+# run guard evaluate
 status_code, result = client.evaluate(messages)
 
 logging.info(f"Evaluation result: {result.get('evaluation').get('status')}")
