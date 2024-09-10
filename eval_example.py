7d07e8a5623b7bceaecd12a20be4e213c711adfb
rafaelsandroni <rafaelsandroni@users.noreply.github.com>
2024-09-10 17:29:54 +0000

    update API calls

--- eval_example.py
+++ eval_example.py
@@ -1,6 +1,6 @@
 
 from datetime import datetime
-from guard import Guard
+from guard import Guard, Policy, PolicyRuleType, PolicyRuleExpected, PolicyTarget, PolicyRule
 import openai
 import os
 import logging
@@ -13,7 +13,23 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("eval")
 logger.setLevel(logging.INFO)
 
-logger.info("Starting the application")
+import argparse
+
+# Create an ArgumentParser object
+parser = argparse.ArgumentParser(description="Process command-line arguments.")
+
+# Add an argument for loading a file
+policies_ids = "block-pii-exposure, do-not-mention, follow-negotiation, detect-direct-prompt-injection"
+
+parser.add_argument('--load', type=str, help='Specify the file to load.', default=None)
+parser.add_argument('--policy-ids', type=str, help='Specify the file to load.', default=policies_ids)
+parser.add_argument('--add-policy', type=str, help='Specify the file to load.', default=False)
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
+
+# logger.info("Starting the application")
 
 client = Guard(api_key=os.getenv("METATEXT_API_KEY"))
 
@@ -21,93 +37,207 @@ def call_llm(messages):
   response = openai.chat.completions.create(
     model="gpt-4o-mini",
     messages=messages,
-    max_tokens=100
+    max_tokens=1000
   )
   return response.choices[0].message.content
 
-# # read application from file
-# with open('application.json') as f:
-#     application = json.loads(f.read())
-#     application_id = application.get("application_id")
-#     system_prompt = application.get("system_prompt")
-#     client.add_application(application_id=application_id, system_prompt=system_prompt)
-    
-#     for p in application.get("policies", []):
-#         client.add_policy(
-#             policy_id=p.get("id"),
-#             rule=p.get("rule"),
-#             examples=[e for e in p.get("example")]
-#         )
+# Check if the --load argument was provided
+if args.load:
+    # read application from file
+    with open(args.load) as f:
+        application = json.loads(f.read())
+        application_id = application.get("application_id")
+        system_prompt = "\n".join([message.get("content") for message in application.get("messages", []) if message.get("role") == "system"])
+        #system_prompt = application.get("system_prompt")
+        client.add_application(name=application_id, system_prompt=system_prompt)
+        
+        for p in application.get("policies", []):
+            client.add_policy(
+                policy_id=p.get("id"),
+                rule=p.get("rule"),
+                examples=p.get("examples")
+            )
 
-#     logger.info(f"Added {application.get("policies", [])} policies")
-
-# set the current time
+application_id = "YOUR_APPLICATION_NAME10"
 now = datetime.now()
 time_of_day = now.strftime("%I:%M %p")
+system_prompt = f"""
+You're a helfull assistant. Current time is {time_of_day}. 
 
-# set up the application first
-application_id = "YOUR_APPLICATION_ID"
-system_prompt = f"You're a helfull assistant. Current time is {time_of_day}. You work for ACME company. Act as a customer support agent to help users to create an investment account."
-client.add_application(application_id=application_id, system_prompt=system_prompt)
+You work for ACME company. 
 
-policy_1 = dict(
-    policy_id="greetings",
-    rule="""Always say greetings depending on the time of day:
-    After 1:00 AM and before 12:00 PM, say "Good morning."
-    After 12:00 PM and before 6:00 PM, say "Good afternoon."
-    After 6:00 PM and before 9:00 PM, say "Good evening."
-    After 9:00 PM and before 1:00 AM, say "Good night.""",
-    examples=[dict(
-            output_example="Saying 'Good morning!' when current time is after 6:00PM", # could add example as an instruction
-            type="FAIL"
+Act as a customer support agent to help user to negotiate their debt.
+
+Current customer is John, their debit is due and you need to convince him to pay it following the payment conditions:
+
+Conditions: 1x of R$300 or 3x of R$300.
+
+Do not accept or suggest anything other them the conditions. Discounts are not allowed.
+"""
+model = "gpt-4o-mini"
+
+if args.add_policy:
+    # set the current time
+    
+
+    # set up the application first
+    
+    #application_id = client.add_application(name=application_id, system_prompt=system_prompt, model=model)
+    client.set_application(id=application_id, name=application_id)
+
+    # policy_1 = dict(
+    #     policy_id="greetings",
+    #     rule="""Always say greetings depending on the time of day:
+    #     After 1:00 AM and before 12:00 PM, say "Good morning."
+    #     After 12:00 PM and before 6:00 PM, say "Good afternoon."
+    #     After 6:00 PM and before 9:00 PM, say "Good evening."
+    #     After 9:00 PM and before 1:00 AM, say "Good night.""",
+    #     examples=[dict(
+    #             output_example="Saying 'Good morning!' when current time is after 6:00PM", # could add example as an instruction
+    #             type="FAIL"
+    #         ),
+    #         dict(
+    #             output_example="Hi!", # or as a message example
+    #             type="FAIL"
+    #         ),
+    #         dict(
+    #             output_example="Hi, good evening!", # another message example
+    #             type="PASS"
+    #         )   
+    #     ],
+    #     override_response=None, # could add a default response here, when it policy fails, this response is returned
+    # )
+
+    policy_list = [
+
+        
+        Policy(
+            id="detect-direct-prompt-injection",
+            description="",
+            rules=[
+                PolicyRule(
+                    type=PolicyRuleType.JAILBREAK,
+                    expected=PolicyRuleExpected.FAIL,
+                    value="",
+                    threshold=0.8,
+                ),
+            ],
+            target=PolicyTarget.INPUT
         ),
-        dict(
-            output_example="Hi!", # or as a message example
-            type="FAIL"
-        ),
-        dict(
-            output_example="Hi, good evening!", # another message example
-            type="PASS"
-        )   
-    ],
-    override_response=None, # could add a default response here, when it policy fails, this response is returned
-)
 
-# then set up the policy
-for p in [policy_1]:
-    client.add_policy(
-        **p
-    )
+        
+
+        Policy(
+            id="follow-negotiation",
+            description="",
+            rules=[
+                PolicyRule(
+                    type=PolicyRuleType.RUBRIC,
+                    expected=PolicyRuleExpected.FAIL,
+                    value="Você deve trabalhar somente as condições de pagamento disponíveis, 1x de R$300 ou 3x de R$ 100. Não aceite ou sugira outros termos. Sem descontos.",
+                    threshold=0.3, # 0.0 very strict, 1.0 would be more lenient
+                ),
+            ],
+            target=PolicyTarget.OUTPUT
+        ),
+
+        
+
+        Policy(
+            id="do-not-mention",
+            description="",
+            rules=[
+                PolicyRule(
+                    type=PolicyRuleType.CLASSIFIER,
+                    expected=PolicyRuleExpected.FAIL,
+                    value="Itaú, Bradesco, Santander", # class name or "class_name1, class_name2"
+                    threshold=0.7
+                ),
+                PolicyRule(
+                    type=PolicyRuleType.SIMILARITY,
+                    expected=PolicyRuleExpected.FAIL,
+                    value="concorrentes",
+                    threshold=0.8
+                ),
+                PolicyRule(
+                    type=PolicyRuleType.FACTUALITY,
+                    expected=PolicyRuleExpected.FAIL,
+                    value="Concorrentes do Nubank ou outras empresas de serviços financeiros",
+                    threshold=0.8
+                ),
+            ],
+            target=PolicyTarget.OUTPUT
+        ),
+
+        Policy(
+            id="do-not-talk-about-politics",
+            description="",
+            rules=[
+                PolicyRule(
+                    type=PolicyRuleType.CLASSIFIER,
+                    expected=PolicyRuleExpected.FAIL,
+                    value="politics", # class name or "class_name1, class_name2"
+                    threshold=0.5
+                ),
+            ],
+            target=PolicyTarget.BOTH
+        ),
+
+        Policy(
+            id="block-pii-exposure",
+            description="",
+            rules=[
+                PolicyRule(
+                    type=PolicyRuleType.PII,
+                    expected=PolicyRuleExpected.FAIL,
+                    value="email, telefone, endereço empresa, chave pix, senha, password", # add one or many labels
+                    threshold=0.7
+                ),
+            ],
+            target=PolicyTarget.OUTPUT
+        )
+    ]
+
+    # then set up the policy
+    for p in policy_list:
+        client.add_policy(
+            p
+        )
 
 # user input
-user_input = input("User Input (Leave empty for default): ")
-if user_input:
-    messages = [
-        {"role": "user", "content": user_input},
-    ]
-else:
-    messages = [
-        {"role": "user", "content": "Hi! I would like to create an investment account."},
-    ]
+messages = [{"role": "system", "content": system_prompt}]
 
-logger.info(f"User input: {json.dumps(messages[-1], indent=4)}")
+while True:
+    user_input = input("User Input (Leave empty for default): ")
+    if user_input:
+        messages += [
+            {"role": "user", "content": user_input},
+        ]
+    else:
+        messages += [
+            {"role": "user", "content": "Olá! Vi que meu nome tá negativado, gostaria de quitar essa dívida, mas desempregado e não tenho mais de R$50 para pagar hoje."},
+        ]
 
-# generate an ai response
-assistant_response = call_llm(messages)
+    # logger.info(f"Messages: {json.dumps(messages, indent=4)}")
+    logger.info(f"User input: {json.dumps(messages[-1], indent=4)}")
 
-messages.append({"role": "assistant", "content": assistant_response})
+    # generate an ai response
+    assistant_response = call_llm(messages)
 
-logger.info(f"Assistant output: {json.dumps(messages[-1], indent=4)}")
+    messages.append({"role": "assistant", "content": assistant_response})
 
-# run guard evaluate
-status_code, result = client.evaluate(messages)
+    logger.info(f"Assistant output: {json.dumps(messages[-1], indent=4)}")
 
-logger.info(f"Evaluation result: {result.get('evaluation').get('status')}")
-if result.get("evaluation").get("status") == "FAIL":
-    [logger.info(f"Policy violations: \n{json.dumps(r, indent=4)}") for r in result.get("evaluation").get("policy_violations")]
-  
-    if result.get("correction"):
-        correction_choices = result.get("correction").get("choices", [])
-        messages[-1] = {"role": "assistant", "content": correction_choices[0].get("content")}
+    # run guard evaluate
+    policy_ids = args.policy_ids.split(",") if isinstance(args.policy_ids, str) else [args.policy_ids]
+    status_code, result = client.evaluate(messages, policy_ids=policy_ids, application_id=application_id)
+
+    logger.info(f"Evaluation result: {result.get('status')}")
+    if result.get("status") == "FAIL":
+        [logger.info(f"Policy violations: \n{json.dumps(r, indent=4)}") for r in result.get("policy_violations")]
     
-    logger.info(f"Correction: {json.dumps(messages[-1], indent=4)}")
+        if result.get("correction"):
+            correction_choices = result.get("correction").get("choices", [])
+            messages[-1] = {"role": "assistant", "content": correction_choices[0].get("content")}
+        
+        logger.info(f"Correction: {json.dumps(messages[-1], indent=4)}")
--- guard.py
+++ guard.py
@@ -13,36 +13,103 @@ logger.setLevel(logging.INFO)
 
 load_dotenv()
 
-URL = 'https://guard-api.metatext.ai'
+BASE_URL = 'https://guard-api.metatext.ai'
 EVALUATE = '/v1/evaluate'
+APPLICATION = '/v1/applications'
+POLICIES = '/v1/applications/{application_id}/policy'
+
 API_KEY = os.getenv("METATEXT_API_KEY")
+
+from pydantic import BaseModel, Field
+from enum import Enum
+from typing import List, Dict, Optional
+
+
+class PolicyTarget(str, Enum):
+    INPUT = "input"
+    OUTPUT = "output"
+    BOTH = "both"
+    
+class PolicyRuleType(str, Enum):
+    FACTUALITY = "factuality"
+    RUBRIC = "rubric"
+    CLASSIFIER = "classifier"
+    SIMILARITY = "similarity"
+    REGEX = "regex"
+    CONTAINS = "contains"
+    PII = "pii"
+    JAILBREAK = "jailbreak"
+
+class PolicyRuleExpected(str, Enum):
+    PASS = "pass"
+    FAIL = "fail"
+
+class PolicyRule(BaseModel):
+    type: PolicyRuleType
+    expected: PolicyRuleExpected
+    value: Optional[str] = None
+    threshold: Optional[float] = None
+
+class Policy(BaseModel):
+    id: str
+    description: str
+    rules: List[PolicyRule]
+    target: PolicyTarget = PolicyTarget.BOTH
+    override_response: Optional[str] = None
+
 
 # client
 class Guard:
     def __init__(self, api_key):
       
-        self.url = URL+EVALUATE
+        self.base_url = BASE_URL
+        self.endpoints = {
+            "evaluate": EVALUATE,
+            "application": APPLICATION,
+            "policy": POLICIES
+        }
         self.headers = {
             'accept': 'application/json',
             'Authorization': f'Bearer {api_key}',
             'Content-Type': 'application/json',
         }
-        self.override_policy = []  # Initialize an empty list for policies
+        self.policy = []
+        self.application = None
 
-    def add_application(self, application_id, system_prompt = None):
+    def request(self, endpoint, data):
+        with httpx.Client(timeout=None) as client:
+            response = client.post(endpoint, headers=self.headers, json=data)
+            result = response.json()
+            assert response.status_code < 500, f"Failed to receive response: {result}"
+            return response.status_code, result
+
+    def set_application(self, id, name):
+        self.application = dict(id=id, name=name)
+
+    def add_application(self, name, system_prompt = None, model = None):
         """
         Adds an application to the Metatext API.
         
         :param application_id: Unique ID for the application.
         :param system_prompt: LLM instructions.
         """
-        application = {
-            "id": application_id,
-            "system_prompt": system_prompt
+        data = {
+            "name": name,
+            "description": "",
+            "model": model,
+            "parameters": {"system_prompt": system_prompt}
         }
-        self.application = application
 
-    def add_policy(self, policy_id, rule, examples, override_response=None):
+        endpoint = self.base_url + '/v1/applications'
+        _, application = self.request(endpoint, data)
+        print(_, application)
+        
+        self.application = application
+        logger.info(f"Added application: {application.get('id')}")
+        return application.get("id")
+        
+
+    def add_policy(self, policy: Policy):
         """
         Adds a policy to the override_policy list.
 
@@ -51,22 +118,19 @@ class Guard:
         :param example: A list of example dictionaries showing how the policy should behave.
         :param override_response: Optional response to override when the policy triggers.
         """
-        policy = {
-            "rule": rule,
-            "examples": examples,
-            "id": policy_id
-        }
-        if override_response:
-            policy["override_response"] = override_response # override response if this policy is violated
         
-        self.override_policy.append(policy)
-        logger.info(f"Added policy: {policy_id} Total: {len(self.override_policy)}")
+        assert self.application.get('id'), "Application ID not defined"
+        endpoint = self.base_url + f"/v1/applications/{self.application.get('id')}/policies"
+        
+        _, new_policy = self.request(endpoint, policy.model_dump())
+        self.policy += new_policy
+        logger.info(f"Added policy: {new_policy.get('id')} Total: {len(self.policy)}")
     
-    def evaluate(self, messages, threshold=0.3, correction_enabled=True, override_response=None, fail_fast=False, policy_ids=None):
+    def evaluate(self, messages, threshold=0.3, correction_enabled=True, override_response=None, fail_fast=False, policy_ids=None, application_id=None):
         if policy_ids is None:
             policy_ids = []
 
-        assert self.application, "Please add an application before evaluating messages"
+        
         assert len(messages) > 0, "Please provide at least one message to evaluate"
         assert messages[-1].get("role") == "assistant", "The last message should be from the assistant"
         assert messages[-2].get("role") == "user", "The last last message should be from user"
@@ -74,22 +138,40 @@ class Guard:
         if messages[0].get("role") != "system":
             messages.insert(0, {"role": "system", "content": self.application.get("system_prompt")})
         
+        application_id = application_id if application_id else self.application.get("id")
+        assert application_id, "Please add an application id before evaluating messages"
         data = {
             "messages": messages,
-            "application": self.application.get("id"),
-            "threshold": threshold,
+            "application": application_id,
             "correction_enabled": correction_enabled,
             "fail_fast": fail_fast,
-            "policy_ids": policy_ids,
-            "override_policy": self.override_policy,
-            "override_response": override_response # override if any policy is violated
+            "policy_ids": policy_ids
         }
-
-        with httpx.Client(timeout=None) as client:
-            response = client.post(self.url, headers=self.headers, json=data)
-            result = response.json()
-            assert response.status_code == 200 or result is not None, f"Failed to evaluate messages: {result}"
-            return response.status_code, result
+        endpoint = self.base_url +'/v1/evaluate'
+        _, result = self.request(endpoint, data)
+        print(result)
+        return _, result
 
 # example usage
 client = Guard(api_key=API_KEY)
+
+
+# policy = [
+#     Policy(
+#         id="block-direct-injection",
+#         rules=[
+#             PolicyRule(
+#                 type=PolicyRuleType.classifier,
+#                 expected=PolicyRuleExpected.FAIL
+#                 value="extrair dados"
+#                 threshold=0.7
+#             ),
+#             PolicyRule(
+#                 type=PolicyRuleType.jailbreak,
+#                 expected=PolicyRuleExpected.FAIL
+#                 threshold=0.8
+#             ),
+#         ],
+#         target=PolicyTarget.INPUT
+#     )
+# ]
